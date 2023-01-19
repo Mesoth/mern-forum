bf263d2e9de6171b922219b48755c6a74332a48c
Mesoth <lalemberger@gmail.com>
2023-01-19 00:30:43 -0300

    Add use effect for fetching the topics

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import './App.css';
 
 function App() {
+
+  const [topics, setTopics] = useState([])
+
+  useEffect(() => {
+    async function fetchTopics(){
+      const response = await fetch('http://localhost:3000/topics')
+      const newTopics = await response.json()
+      setTopics(newTopics)
+    }
+    fetchTopics()
+  }, [])
+
   return (
     <div className="App">
       <h1 className="title">Welcome to your favorite manga forum</h1>
