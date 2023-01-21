50662b0751f663274aea0c5c0def8b4f23e0bb09
Mesoth <lalemberger@gmail.com>
2023-01-21 20:49:16 -0300

    Add react router and charging screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import './index.css'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
+import "./App.css";
 
 function useFetchTopics() {
-  const [topics, setTopics] = useState([])
-  
-  useEffect(() => {
-    async function fetchTopics(){
-      const response = await fetch('http://localhost:5000/topics')
-      const newTopics = await response.json()
-      setTopics(newTopics)
-    }
-    fetchTopics()
-  }, [])
+  const [topics, setTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  return topics
+  useEffect(() => {
+    async function fetchTopics() {
+      const response = await fetch("http://localhost:5000/topics");
+      const newTopics = await response.json();
+      setTopics(newTopics);
+      setIsLoading(false);
+    }
+    fetchTopics();
+  }, []);
+
+  return { topics, isLoading };
 }
 
 function App() {
-
-  const topics = useFetchTopics()
+  const { topics, isLoading } = useFetchTopics();
 
   return (
     <div className="App">
-      <h1 className="title">Welcome to your favorite manga forum</h1>
-      <ul className="topics-container">
-        {topics.map( topic => {
-          return <li className="topic" key={topic._id}>{topic.title}</li>
-        })}
-      </ul>
+      {isLoading ? (
+        <div className="carga-container">
+          <h1>Cargando...</h1>
+          <div class="custom-loader"></div>
+        </div>
+      ) : (
+        <div>
+          <h1 className="title">Welcome to your favorite manga forum</h1>
+          <ul className="topics-container">
+            {topics.map((topic) => {
+              return (
+                <li key={topic._id}>
+                  <Link className="topic" to={topic._id}>
+                    {topic.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
---
+++ client/src/Components/Topic.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+
+export default function Topic() {
+  return <h1>lo lograste!!</h1>;
+}
