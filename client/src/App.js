d9817b0b79e8bc703549cfaf180288698c4be6b1
Mesoth <lalemberger@gmail.com>
2023-01-20 21:54:46 -0300

    Add topics to the UI and refactor according to single responsability principle

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import './index.css';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './index.css'
+import './App.css'
 
-function App() {
-
+function useFetchTopics() {
   const [topics, setTopics] = useState([])
-
+  
   useEffect(() => {
     async function fetchTopics(){
-      const response = await fetch('http://localhost:3000/topics')
+      const response = await fetch('http://localhost:5000/topics')
       const newTopics = await response.json()
       setTopics(newTopics)
     }
     fetchTopics()
   }, [])
 
+  return topics
+}
+
+function App() {
+
+  const topics = useFetchTopics()
+
   return (
     <div className="App">
       <h1 className="title">Welcome to your favorite manga forum</h1>
+      <ul className="topics-container">
+        {topics.map( topic => {
+          return <li key={topic._id}>{topic.title}</li>
+        })}
+      </ul>
     </div>
   );
 }
